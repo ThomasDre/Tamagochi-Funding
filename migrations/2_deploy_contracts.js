42e15c58ab4df09a4535ace3bdbd81f177a17b35
ThomasDre <drescher.thomas@outlook.com>
2021-06-06 13:22:20 +0200

    Update deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,8 +6,8 @@ const Fundraising = artifacts.require("Fundraising");
 module.exports = function(deployer) {
     deployer.deploy(Board).then(function() {
         return deployer.deploy(Tamagochi, Board.address);
-    });
+    })
+    
     // TODO this deplyoment is only meant for dev (convenience of getting an abi for testing)
-    deployer.deploy(Fundraising);
-
+    //deployer.deploy(Fundraising);
 };
